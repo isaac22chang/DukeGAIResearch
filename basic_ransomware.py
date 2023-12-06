c6a009f5f83bc3f7c16409876760d90a36c75189
isaac22chang <59808652+isaac22chang@users.noreply.github.com>
2023-12-05 21:33:24 -0800

    Add files via upload

--- basic_ransomware.py
+++ basic_ransomware.py
@@ -53,3 +53,9 @@ install_libraries(first_line)
 print("went thru")
 subprocess.run(["python", filename])
 
+try:
+    result = subprocess.run(["python", filename], capture_output=True, text=True, check=True)
+    print("Script output:", result.stdout)
+except subprocess.CalledProcessError as e:
+    print("Error running script:", e)
+    print("Script output (stderr):", e.stderr)
